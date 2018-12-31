68cab9284c60f2c830a01993cd0f988b427969cb
nice fontaine <ulla.umme@gmail.com>
2018-12-31 19:09:52 +0100

    preset changes every 2 min the shader

--- app.py
+++ app.py
@@ -3,15 +3,20 @@ import _thread
 import socket
 import time
 import markdown2
+from multiprocessing import Process
+import random
+import urllib.request
 from random import random
 from random import randint
 from flask import Flask
 from flask import request
 
+
 app = Flask(__name__)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 SERVER_IP = "192.168.1.9"
 UDP_PORT = 8888
+SHADER_PRESET_INTERVAL = 120
 ADDRESSES = [
     "127.0.0.1",
     "192.168.1.10",
@@ -55,7 +60,7 @@ def toggle_screen():
     package = bytes([1, int(on)])
     address = ADDRESSES[int(screen)]
     sock.sendto(package, (address, UDP_PORT))
-    return f"Turned Screen {screen} on: {bool(int(on))}"
+    return "Turned Screen {} on: {}".format(screen, bool(int(on)))
 
 
 @app.route("/change_shader")
@@ -75,7 +80,7 @@ def change_shader():
     package = bytes([1, int(shader)])
     for address in ADDRESSES:
         sock.sendto(package, (address, UDP_PORT))
-    return f"Changed all screens to shader {shader}"
+    return "Changed all screens to shader {}".format(shader)
 
 
 @app.route("/reset_time")
@@ -94,7 +99,7 @@ def reset_time():
     package = bytes([2])
     sock.sendto(package, (address, UDP_PORT))
 
-    return f"Reset time of Screen {screen}"
+    return "Reset time of Screen {}".format(screen)
 
 
 @app.route("/reset_times")
@@ -111,7 +116,7 @@ def reset_times():
         return "Please provide an info if you want to sync times"
     synced = bool(int(synced))
     _thread.start_new_thread(reset_times_of_addresses, (synced,))
-    return f"Sync time off all screens: {synced}"
+    return "Sync time off all screens: {}".format(synced)
 
 
 def reset_times_of_addresses(synced):
@@ -141,7 +146,7 @@ def screen_span():
         tmp = mode if mode < 2 else randint(0, 1)
         package = bytes([3, tmp])
         sock.sendto(package, (address, UDP_PORT))
-    return f"Changed screen spans to {modes[mode]}"
+    return "Changed screen spans to {}".format(modes[mode])
 
 
 @app.route("/manual")
@@ -158,11 +163,26 @@ def manual():
     if mode is None:
         return "Please provide a mode"
     mode = int(mode)
-    for address in ADDRESSES:
-        package = bytes([4, mode])
-        sock.sendto(package, (address, UDP_PORT))
-    return f"Changed mode to {modes[mode]}"
+    if mode == 0:
+        if queue[0].is_alive():
+            queue[0].terminate()
+        queue.append(Process(target=change_shader_worker, args=(SHADER_PRESET_INTERVAL,)))
+        if len(queue) > 1:
+            del queue[0]
+    else:
+        if not queue[0].is_alive():
+            queue[0].start()
+    return "Changed mode to {}".format(modes[mode])
+
+
+def change_shader_worker(interval):
+    while True:
+        shader = randint(1, 17)
+        address = "http://{}:5000/change_shader?shader={}".format(SERVER_IP, shader)
+        urllib.request.urlopen(address).read()
+        time.sleep(interval)
 
 
 if __name__ == '__main__':
+    queue = [Process(target=change_shader_worker, args=(SHADER_PRESET_INTERVAL,))]
     app.run(host=SERVER_IP, port=5000)
